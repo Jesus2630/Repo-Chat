9f00916e9012010ba862b882b137512582418932
Luna <www.lunabenitez@gmail.com>
2022-11-29 12:00:44 -0300

    input user y pequeños cambios

--- public/js/emojis.js
+++ public/js/emojis.js
@@ -1,8 +1,14 @@
+//Variables emojis
 const cajaEmojis = document.querySelector("#container_emojis");
 const emojis = document.querySelectorAll("#emoji");
 const botonEmoji = document.querySelector("#botonEmoji");
 const input = document.querySelector('#cuadroMensaje');
+//Variables stickers
+const cajaStickers = document.querySelector("#container_stickers");
+const stickers = document.querySelectorAll("#sticker");
+const scuadroMensaje  = document.querySelector('#cuadroMensaje');
 
+//emoji
 botonEmoji.addEventListener("click", () => {
     cajaEmojis.classList.toggle("open_container_emojis")
 });
@@ -13,4 +19,19 @@ for(const emojisFor of emojis){
     });
 };
 
+//sticker
+// botonEmoji.addEventListener("click", () => {
+//    cajaStickers.classList.toggle("open_container_stickers")
+//});
 
+for(const stickersFor of stickers){
+    stickersFor.addEventListener("click", () => {
+        scuadroMensaje.value += stickersFor.innerHTML;
+    });
+};
+
+/* for(const stickersFor of stickers){
+    stickersFor.addEventListener("click", () => {
+        scajaMensajes.innerHTML += stickersFor.innerHTML;
+    });
+}; */
