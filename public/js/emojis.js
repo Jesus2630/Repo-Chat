78ef0545aa8d4b7d8833e95bd8b4720a17dbe154
Jesus Farias <jesus.fg2630@gmail.com>
2022-11-30 02:13:21 -0300

    Config emojis

--- public/js/emojis.js
+++ public/js/emojis.js
@@ -1,14 +1,25 @@
+/* Codigo de reserva */
+
+
 //Variables emojis
 /* const cajaEmojis = document.querySelector("#container_emojis");
 const emojis = document.querySelectorAll("#emoji");
 const botonEmoji = document.querySelector("#botonEmoji");
 const input = document.querySelector('#cuadroMensaje'); */
 //Variables stickers
-/* const cajaStickers = document.querySelector("#container_stickers");
+const cajaStickers = document.querySelector("#container_stickers");
 const stickers = document.querySelectorAll("#sticker");
-const scuadroMensaje  = document.querySelector('#cuadroMensaje');
-const scajaMensajes  = document.querySelector('#caja-mensajes');
- */
+const scuadroMensaje  = document.querySelector('#txtMensaje');
+const scajaMensajes  = document.querySelector('#divChatbox');
+
+var params = new URLSearchParams(window.location.search);
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+ 
 //emoji
 /* botonEmoji.addEventListener("click", () => {
     cajaEmojis.classList.toggle("open_container_emojis")
@@ -21,15 +32,16 @@ for(const emojisFor of emojis){
 }; */
 
 //sticker
-/* botonEmoji.addEventListener("click", () => {
-cajaStickers.classList.toggle("open_container_stickers")
-}); */
-/* 
+botonEmoji.addEventListener("click", () => {
+    cajaStickers.classList.toggle("open_container_stickers")
+});
+
+
 for(const stickersFor of stickers){
     stickersFor.addEventListener("click", () => {
-        scuadroMensaje.value += stickersFor.innerHTML;
+        scajaMensajes.innerHTML += `${nombre}:  ${stickersFor.innerHTML}`;
     });
-}; */
+}; 
 
 /* for(const stickersFor of stickers){
     stickersFor.addEventListener("click", () => {
